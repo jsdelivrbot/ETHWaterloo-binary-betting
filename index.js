542e0a8d20e9794aae0f85a307fca0c655e5d762
Ken Suong <kensuong@gmail.com>
2017-10-14 15:33:38 -0400

    NodeJS server is able to generate a private key

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const rp = require('request-promise')
 const exphbs = require('express-handlebars')
 const path = require('path')
 const request = require('request')
-const Web3 = require('web3')
+const Web3 = require('Web3')
 
+// set up handlebars so variables are rendered onto the homepage 
 app.engine('.hbs', exphbs({
   defaultLayout: 'main',
   extname: '.hbs',
@@ -15,16 +16,13 @@ app.engine('.hbs', exphbs({
 app.set('view engine', '.hbs')
 app.set('views', path.join(__dirname, 'views'))
 
-
+// set up the port on localhost:3000
 app.listen(port, (err) => {
   if (err) {
     return console.log('something bad happened', err)
   }
-
   console.log(`server is listening on ${port}`)
 })
-
-var web3 = new Web3(Web3.givenProvider || "ws://lcoalhost:8546")
 
 var asset_name = ""
 var asset_previous_date = "Oct 7, 2017 18:27:00 UTC" // TODO: Replace fake data
@@ -35,6 +33,7 @@ var winner = ""
 
 //TODO: Build the loop that will check for duration of time
 
+// calls the coindesk API 
 request('https://api.coindesk.com/v1/bpi/currentprice.json', function (error, response, body) {
 	if (!error && response.statusCode == 200) {
 		coindesk_current = JSON.parse(body)
@@ -64,3 +63,8 @@ app.get('/', (request, response) => {
     bet_winner: winner
   })
 })
+
+// generate the private key from a passphrase 
+var web3 = new Web3()
+var privateKeyRaw = web3.sha3("Have you ever seen too much as to go to there")
+console.log(privateKeyRaw)
